3811158cddab80eee996e25270588c7848d34b44
purt85 <khachikpurt85@gmail.com>
2021-11-25 17:33:18 +0400

    024

--- script.js
+++ script.js
@@ -1,67 +1,95 @@
 //'use strict';
 
-let NumOfFilms;
+// let NumOfFilms;
 
-function start() {
-    NumOfFilms = +prompt("qani kino", "");
+// //function 
 
-    while (NumOfFilms == '' || NumOfFilms == null || isNaN(NumOfFilms)) {
-        NumOfFilms = +prompt('qani kino', '');
-    }
-}
-
-start();
+// start();
 
 const personalMovieDB = {
-    count: NumOfFilms,
+
+    count: 0,
     movies: {},
     actors: {},
     geners: [],
-    privat: false
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt("qani kino", "");
+
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('qani kino', '');
+        }
+    },
+    rememberMyFilms: () => {
+        for (let i = 0; i < 1; i++) {
+            const a = prompt(" kinoneric mek@", ""),
+                b = prompt("inchqan kgnahateq?", "");
+            if (a != null && b != null && a != "" && b != "" && a.length < 20) {
+                personalMovieDB.movies[a] = b;
+                console.log("done");
+            } else {
+                console.log("Err");
+                i--;
+            }
+
+        }
+    },
+    PersLevel: () => {
+        if (personalMovieDB.count < 10) {
+            console.log(" qicha ");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+            console.log(" normala ");
+        } else if (personalMovieDB.count > 30) {
+            console.log(" shata ");
+        } else {
+            console.log(" errooooor ");
+        }
+    },
+    
+    showMyDB: (hidden) => {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function(){
+        if (personalMovieDB.privat){
+            personalMovieDB.privat = false;
+
+        } else {
+            personalMovieDB.privat=true;
+        }
+    },
+    writeYourGeners: () => {
+        for (let i = 1; i <= 3; i++) {
+            // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            
+            // if(genre === '' || genre == null) {
+            //     console.log('sxal tvyal kam tvyal chka');
+            //     i--;
+            // } else {
+            //     personalMovieDB.geners[i - 1] = genre;
+            // }
+            let genre = prompt(`mutqagreq stwraketov ${i}`);
+
+        }
+        personalMovieDB.geners.forEach((item, i) => {
+            console.log(`Любимый жанр $(i+1) это $(название из массива)`);
+
+        });
+
+    }
+
 };
 
-function rememberMyFilms() {
-    for (let i = 0; i < 1; i++) {
-        const a = prompt(" kinoneric mek@", ""),
-            b = prompt("inchqan kgnahateq?", "");
-        if (a != null && b != null && a != "" && b != "" && a.length < 20) {
-            personalMovieDB.movies[a] = b;
-            console.log("done");
-        } else {
-            console.log("Err");
-            i--;
-        }
-
-    }
-}
 //rememberMyFilms();
 
-function PersLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log(" qicha ");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-        console.log(" normala ");
-    } else if (personalMovieDB.count > 30) {
-        console.log(" shata ");
-    } else {
-        console.log(" errooooor ");
-    }
-}
+
 //PersLevel();
 
-function showMyDB(hidden){
-    if(!hidden){
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
 
-function writeYourGeners(){
-    for(let i = 1; i <= 3; i ++) {
-        personalMovieDB.geners[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-writeYourGeners();
+//showMyDB(personalMovieDB.privat); naxkiun stex er kanchec
+
+
+//writeYourGeners();
 
 // esa
-
